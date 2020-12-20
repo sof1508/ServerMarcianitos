085f0025ccfe8ce68cb23ad2a6b46dee62cf6fc8
sof1508 <sandritaorenguito@gmail.com>
2020-12-20 12:43:10 +0100

    retoque

--- model/revision.js
+++ model/revision.js
@@ -3,7 +3,7 @@ const x = {}
 
 //Listar revisiones de cierta Aeronave
 x.get_revisionesAeronave = function(idAeronave) {
-    //return query('SELECT revision.ID, revision.Fecha, revision.AeronaveRevisada, `historial marciano`.Marciano FROM revision JOIN `historial marciano` ON `historial marciano`.Revision = revision.ID WHERE revision.AeronaveRevisada = ?', [
+    //return query('SELECT revision.ID, revision.Fecha, revision.AeronaveRevisada, `historial marciano`.Marciano FROM revision JOIN `aeronave` ON `historial marciano`.Revision = revision.ID WHERE revision.AeronaveRevisada = ?', [
     return query('SELECT ID as id, nombreRevisor, Fecha as fecha, AeronaveRevisada as idAeronave FROM revision WHERE revision.AeronaveRevisada = ?', [
         idAeronave
     ])
@@ -11,8 +11,8 @@ x.get_revisionesAeronave = function(idAeronave) {
 
 //Listar revisiones 
 x.get_revisiones = function() {
-    //return query('SELECT revision.ID, revision.AeronaveRevisada, revision.Fecha, `historial marciano`.Marciano FROM revision JOIN `historial marciano` ON `historial marciano`.Revision = revision.ID', [
-    return query('SELECT ID as id, nombreRevisor, Fecha as fecha, AeronaveRevisada as idAeronave FROM revision', [])
+    return query('SELECT revision.ID as id, revision.nombreRevisor, revision.Fecha as fecha, revision.AeronaveRevisada as idAeronave, aeronave.Nombre as nombre FROM revision JOIN aeronave ON aeronave.ID = revision.AeronaveRevisada', [])
+        //return query('SELECT ID as id, nombreRevisor, Fecha as fecha, AeronaveRevisada as idAeronave FROM revision', [])
 }
 
 //Crear revision
